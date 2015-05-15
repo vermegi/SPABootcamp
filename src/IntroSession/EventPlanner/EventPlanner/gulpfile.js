844f010ce685d6765cf77fddb3dfb9121663da04
vermegi <gitte.vermeiren@gmail.com>
2015-05-15 11:40:19 +0200

    added bootstrap

--- src/IntroSession/EventPlanner/EventPlanner/gulpfile.js
+++ src/IntroSession/EventPlanner/EventPlanner/gulpfile.js
@@ -1,5 +1,23 @@
-﻿var gulp = require('gulp');
+﻿var gulp = require('gulp'),
+    del = require('del');
 
-gulp.task('default', function () {
+var paths = {
+    bower: "./bower_components/",
+    lib: "./Content/lib/"
+};
+
+gulp.task('copy', function() {
+    var bower = {
+        "bootstrap": "bootstrap/dist/**/*.{js,map,css,ttf,svg,woff,eot}",
+        "jquery": "jquery/dist/jquery*.{js,map}",
+    }
+
+    for (var destinationDir in bower) {
+        gulp.src(paths.bower + bower[destinationDir])
+          .pipe(gulp.dest(paths.lib + destinationDir));
+    }
+});
+
+gulp.task('default', ['copy'], function () {
     // place code for your default task here
 });
