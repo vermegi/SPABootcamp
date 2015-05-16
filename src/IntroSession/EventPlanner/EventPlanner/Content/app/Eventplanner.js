0b631cb51ab0c6535902bb6324d5c3552be2164a
vermegi <gitte.vermeiren@gmail.com>
2015-05-16 16:36:17 +0200

    statisch detail van een evenement

---
+++ src/IntroSession/EventPlanner/EventPlanner/Content/app/Eventplanner.js
@@ -0,0 +1,16 @@
+﻿var eventplanner = angular.module("EventPlanner", []);
+
+eventplanner.controller('detailEvenementCtrl', function ($scope) {
+    $scope.evenement = {
+        titel: "titel van het evenement",
+        id: 123,
+        eigenaar: 'de eigenaar',
+        omschrijving: 'een vreed lange omschrijving van je yada dada',
+        muziekVergunning: true,
+        reservatieData: ["21/10/1980", "30/5/2015", "27/8/2015"]
+    };
+    $scope.detailEvenementOpslaan = function () {
+        
+    };
+});
+
