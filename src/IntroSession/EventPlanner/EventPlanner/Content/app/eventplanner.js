466ee951dd21abec0a3cd53e317df5fd2e993b5a
sanderfalleyn <sander.falleyn@gmail.com>
2015-05-21 11:31:52 +0200

    svc

--- src/IntroSession/EventPlanner/EventPlanner/Content/app/eventplanner.js
+++ src/IntroSession/EventPlanner/EventPlanner/Content/app/eventplanner.js
@@ -1,18 +1,32 @@
 ﻿var eventplanner = angular.module('EventPlanner', []);
 
-eventplanner.controller('DetailEvenementController', function ($scope,$http) {
+eventplanner.controller('DetailEvenementController', function ($scope,ReservatieService) {
     $scope.detailEvenementOpslaan = function () { alert('test'); };
 
     $scope.init = function (evenementId) {
-        $http.get('/Reservatie/GetReservatieData',
-                { params: { evenementId: evenementId } })
-            .success(function(data) {
+        ReservatieService.getReservatieData(evenementId)
+            .then(function (data) {
                 $scope.evenement = data.evenement;
                 $scope.reservatieData = data.reservatieData;
-            })
-            .error(function() {
-                alert('oops', 'I did it again');
             });
+        
         
     };
 });
+
+eventplanner.factory('ReservatieService', function($http, $q) {
+
+    return {
+        getReservatieData: function (evenementId) {
+            var dfd = $q.defer();
+            $http.get('/Reservatie/GetReservatieData',
+                { params: { evenementId: evenementId } })
+                .success(dfd.resolve)
+                .error(function() {
+                    alert('oops', 'I did it again');
+                });
+            return dfd.promise;
+        }
+    };
+});
+
