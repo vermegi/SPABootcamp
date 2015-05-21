04b08f80e7f64e0643eb0f062f76ca7968697bef
sanderfalleyn <sander.falleyn@gmail.com>
2015-05-21 11:28:23 +0200

    voorlopige status

---
+++ src/IntroSession/EventPlanner/EventPlanner/Content/app/eventplanner.js
@@ -0,0 +1,18 @@
+﻿var eventplanner = angular.module('EventPlanner', []);
+
+eventplanner.controller('DetailEvenementController', function ($scope,$http) {
+    $scope.detailEvenementOpslaan = function () { alert('test'); };
+
+    $scope.init = function (evenementId) {
+        $http.get('/Reservatie/GetReservatieData',
+                { params: { evenementId: evenementId } })
+            .success(function(data) {
+                $scope.evenement = data.evenement;
+                $scope.reservatieData = data.reservatieData;
+            })
+            .error(function() {
+                alert('oops', 'I did it again');
+            });
+        
+    };
+});
