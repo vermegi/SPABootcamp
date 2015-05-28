e0ff4f5ca7605b1717552fde78a6b5b0a103d2a2
vermegi <gitte.vermeiren@gmail.com>
2015-05-28 08:30:17 +0200

    added reservatiecontroller api

---
+++ src/IntroSession/EventPlanner/EventPlanner/Api/ReservatieController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+using EventPlanner.Contracts;
+using EventPlanner.Models;
+
+namespace EventPlanner.Api
+{
+    public class ReservatieController : ApiController
+    {
+        public IHttpActionResult GetReservatieData([FromUri]GetEvenementDataRequest request)
+        {
+            using (var ctx = new EvenementEntities())
+            {
+                var reservatie = (from r in ctx.Evenementen
+                    where r.Id == request.EvenementId
+                    select new EvenementDetail
+                    {
+                        Evenement = r,
+                        ReservatieData = r.Periodes.SelectMany<Periode, DateTime>(p => p.Dagen.Select(d => d.Datum))
+
+                    }).SingleOrDefault();
+
+                return Json(reservatie);
+            }
+        }
+    }
+}
