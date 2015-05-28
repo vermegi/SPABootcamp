05cc4cc3ba44b8083031c341b76f772f0c958b48
vermegi <gitte.vermeiren@gmail.com>
2015-05-28 08:27:27 +0200

    added webapi

--- src/IntroSession/EventPlanner/EventPlanner/App_Start/RouteConfig.cs
+++ src/IntroSession/EventPlanner/EventPlanner/App_Start/RouteConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Web.Http;
+using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace EventPlanner
@@ -7,12 +8,32 @@ namespace EventPlanner
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            RegisterWebApiRoutes(routes);
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+
+        private static void RegisterWebApiRoutes(RouteCollection routes)
+        {
+            routes.MapHttpRoute(
+                "ApiControllerOnly",
+                "api/{controller}");
+
+            routes.MapHttpRoute(
+                "ApiControllerAndIntegerId",
+                "api/{controller}/{id}",
+                null,
+                new { id = @"/^\d+$|\b[A-F0-9]{8}(?:-[A-F0-9]{4}){3}-[A-F0-9]{12}\b/i" }); // INT's of GUID's
+
+            routes.MapHttpRoute(
+                "ApiControllerAction",
+                "api/{controller}/{action}"
             );
         }
     }
