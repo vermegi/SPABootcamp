c92753993add49bc14a6219e8fd1a01cb9b4dbcb
vermegi <gitte.vermeiren@gmail.com>
2015-06-03 20:16:34 +0200

    extra methods op api

--- src/IntroSession/EventPlanner/EventPlanner/Api/ReservatieController.cs
+++ src/IntroSession/EventPlanner/EventPlanner/Api/ReservatieController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Web.Http;
+using EventPlanner.Code.Extensions;
 using EventPlanner.Contracts;
 using EventPlanner.Models;
 
@@ -46,6 +48,68 @@ namespace EventPlanner.Api
                 ctx.SaveChanges();
 
                 return Ok();
+            }
+        }
+
+        [HttpPost]
+        public IHttpActionResult PeriodeToevoegen(PeriodeToevoegenCommand command)
+        {
+            using (var ctx = new EvenementEntities())
+            {
+                var periode = new Periode();
+                periode.BeginPeriode = command.BeginDatumPeriode;
+                periode.EindePeriode = command.EindDatumPeriode;
+
+                var straten = (from s in ctx.Straten
+                               where command.GeselecteerdeStraten.Contains(s.Id)
+                               select s);
+
+                periode.Straten = new List<Straat>();
+                foreach (var straat in straten)
+                {
+                    periode.Straten.Add(straat);
+                }
+
+                var dagen = command.GenereerDagen();
+
+                periode.Dagen = new List<Dag>();
+                foreach (var datum in dagen.Where(d => command.DagenToSkip == null || !command.DagenToSkip.Contains(d)))
+                {
+                    var dag = new Dag();
+                    dag.Datum = datum;
+                    periode.Dagen.Add(dag);
+                }
+
+                periode.EvenementId = command.EvenementId;
+
+                ctx.Periodes.Add(periode);
+
+                ctx.SaveChanges();
+
+            }
+
+            return Ok();
+        }
+
+        public IHttpActionResult GetOverlap(GetOverlapRequest request)
+        {
+            var dagen = request.GenereerDagen();
+            var straten = request.GeselecteerdeStraten;
+
+            using (var ctx = new EvenementEntities())
+            {
+                var overlappendeReservaties = (from v in ctx.Evenementen
+                                               join p in ctx.Periodes on v.Id equals p.EvenementId
+                                               where p.Straten.Any(s => request.GeselecteerdeStraten.Contains(s.Id))
+                                               join d in ctx.Dagen on p.Id equals d.PeriodeId
+                                               where dagen.Contains(d.Datum)
+                                               select new { Dag = d.Datum, Reservatie = v, Straten = p.Straten }).ToList();
+
+                var all = (from dag in dagen
+                           join overlappendeReservatie in overlappendeReservaties on dag equals overlappendeReservatie.Dag into reservatie
+                           select new { Dag = dag, Overlappingen = reservatie.Select(res => new Overlapping { Omschrijving = res.Reservatie.Omschrijving, Straat = res.Straten.ToKommaSeparatedString() }) }).ToList();
+
+                return Ok(all);
             }
         }
     }
