a177be0571201d734bef650f941a4f053fbccc59
vermegi <gitte.vermeiren@gmail.com>
2015-06-05 08:51:55 +0200

    last steps

--- src/IntroSession/EventPlanner/EventPlanner/Content/app/eventplanner.js
+++ src/IntroSession/EventPlanner/EventPlanner/Content/app/eventplanner.js
@@ -2,15 +2,6 @@
 
 eventplanner.controller('DetailEvenementCtrl', function($scope, reservatieSvc) {
 
-    $scope.evenement = {
-        titel: 'titel van het evenement',
-        id: 123,
-        eigenaar: 'de eigenaar',
-        omschrijving: 'een vreed lange omschrijving van je yada dada',
-        muziekVergunning: true,
-        reservatieData: ['21/10/1980', '30/5/2015', '27/8/2015']
-    };
-
     $scope.detailEvenementOpslaan = function() {
         reservatieSvc.detailsEvenementOpslaan($scope.evenement);
     };
