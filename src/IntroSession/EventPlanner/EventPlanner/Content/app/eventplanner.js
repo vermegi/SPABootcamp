477178672677561d3e6a49a36a73f2dfe75b2771
vermegi <gitte.vermeiren@gmail.com>
2015-06-05 08:45:16 +0200

    saving an evenement

--- src/IntroSession/EventPlanner/EventPlanner/Content/app/eventplanner.js
+++ src/IntroSession/EventPlanner/EventPlanner/Content/app/eventplanner.js
@@ -1,6 +1,6 @@
 ﻿var eventplanner = angular.module("EventPlanner", []);
 
-eventplanner.controller('DetailEvenementCtrl', function($scope, $http) {
+eventplanner.controller('DetailEvenementCtrl', function($scope, reservatieSvc) {
 
     $scope.evenement = {
         titel: 'titel van het evenement',
@@ -12,18 +12,66 @@ eventplanner.controller('DetailEvenementCtrl', function($scope, $http) {
     };
 
     $scope.detailEvenementOpslaan = function() {
-        
+        reservatieSvc.detailsEvenementOpslaan($scope.evenement);
     };
 
-    $scope.init = function(evenementId) {
-        $http.get('/api/Reservatie/GetReservatieData', { params: { EvenementId: evenementId } })
-            .success(function (data) {
+    $scope.init = function (evenementId) {
+        reservatieSvc.getReservatieData(evenementId)
+            .then(function(data) {
                 $scope.evenement = data.evenement;
                 $scope.reservatieData = data.reservatieData;
-            })
-            .error(function (err) {
-            debugger;
-                alert('something went wrong!');
             });
     };
+});
+
+eventplanner.factory('reservatieSvc', function($http, $q, notifier) {
+    return {
+        getReservatieData: function (evenementId) {
+            var dfd = $q.defer();
+            $http.get('/api/Reservatie/GetReservatieData', { params: { EvenementId: evenementId } })
+                .success(dfd.resolve)
+                .error(function () {
+                    notifier.error('something went wrong!');
+                });
+            return dfd.promise;
+        },
+        detailsEvenementOpslaan: function (evenement) {
+            $http.post("/api/Reservatie/OmschrijvingAanpassen", evenement)
+                .success(function() {
+                    notifier.notify('changes saved succesfully');
+                })
+                .error(function () {
+                    notifier.error("something went wrong");
+                });
+        }
+    }
+});
+
+eventplanner.value('toastr', toastr);
+eventplanner.factory('notifier', function (toastr) {
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    };
+    return {
+        notify: function (msg) {
+            toastr["success"](msg, "");
+        },
+        error: function (msg) {
+            toastr["error"](msg, "");
+        }
+    }
 });
